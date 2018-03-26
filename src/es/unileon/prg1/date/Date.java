db52bb29c155ccc4bbc4d33873b4ccac33074e72
rmartm14 <rmartm14@estudiantes.unileon.es>
2018-03-26 12:50:31 +0200

    Mod_26/03/2018

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -39,7 +39,7 @@ public class Date {
 	*/
 	
 	boolean isSameDay(Date another){
-		if ( this.Day == another.getDay() ){
+		if ( this.day == another.getDay() ){
 			return true;
 		}
 		return false;
@@ -60,7 +60,7 @@ public class Date {
 	}
 	
 	boolean isSame(Date another){
-		if(this.Date.isSameYear==true || this.Date.isSameMonth==true || this.Date.isSameDay==true){
+		if(this.isSameYear(another)==true && this.isSameMonth(another)==true && this.isSameDay(another)==true){
 			return true;
 		}
 		return false;
@@ -71,5 +71,79 @@ public class Date {
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
 	}
-
+	
+	public String getMonthName(){
+		String MonthName;
+		MonthName=" ";
+		switch(this.month){
+			case 1:
+				MonthName= "Enero";
+			break;
+			case 2:
+				MonthName= "Febrero";
+			break;
+			case 3:
+				MonthName= "Marzo";
+			break;
+			case 4:
+				MonthName= "Abril";
+			break;
+			case 5:
+				MonthName= "Mayo";
+			break;
+			case 6:
+				MonthName= "Junio";
+			break;
+			case 7:
+				MonthName= "Julio";
+			break;
+			case 8:
+				MonthName= "Agosto";
+			break;
+			case 9:
+				MonthName= "Septiembre";
+			break;
+			case 10:
+				MonthName= "Octubre";
+			break;
+			case 11:
+				MonthName= "Noviembre";
+			break;			
+			case 12:
+				MonthName= "Diciembre";
+			break;
+			}
+	return MonthName;
+	}
+	
+	public boolean isDayOfTheMonthOk(){
+	boolean ok;
+	ok=false;
+	switch (this.month){
+	case 1:
+	case 3: 
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		if(this.day<=31 && this.day>0){
+		ok=true;
+		}
+	
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		if(this.day<=30 && this.day>0){
+		ok=true;
+		}
+	case 2:
+		if(this.day<=28 && this.day>0){
+		ok=true;
+		}
+	}
+	return ok;
+	}
+	
 }
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -7,6 +7,18 @@ public class MainDate {
 		today = new Date(20, 3, 2018);
 		tomorrow = new Date(21, 3, 2018);
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
+		System.out.println(today + " isSameDay " + tomorrow + "? " + today.isSameDay(tomorrow));
+		System.out.println(today + " isSameMonth " + tomorrow + "? " + today.isSameMonth(tomorrow));
+		System.out.println(today + " isSame " + tomorrow + "? " + today.isSame(tomorrow));
+		System.out.println("El nombre del mes es: " + today.getMonthName());
+		
+		if(today.isDayOfTheMonthOk()==true){
+			System.out.println("El dia del mes es correcto");
+		}
+		else{
+			System.out.println("El dia del mes no es correcto");
+		}
+
 	}
 
 }

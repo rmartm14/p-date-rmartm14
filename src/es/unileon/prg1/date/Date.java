c7152b49be0adbcd5b88bdd82883815138a017f7
rmartm14 <rmartm14@estudiantes.unileon.es>
2018-03-26 11:53:26 +0200

    ADD_comments_METODOS_GET

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -12,16 +12,61 @@ public class Date {
 		this.year = year;
 	}
 	
+	/*
+	*
+	* Nos devuelve los dias, meses y años
+	*
+	* Para cualquier cosa, date.getDay(); es la sintaxis
+	*/
+	
+	
+	public int getDay(){		
+		return this.day;
+	}	
+	
+	public int getMonth(){
+		return this.month;
+	}	
+	
 	public int getYear(){
 		return this.year;
 	}
-
+	
+	/*
+	* Compara la fecha entre diferentes variables: El año, el dia, el mes o todo
+	*
+	* La sintaxis de utilizacion es Date1.isSameDay(otraFecha);
+	*/
+	
+	boolean isSameDay(Date another){
+		if ( this.Day == another.getDay() ){
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isSameMonth(Date another){
+		if ( this.month == another.getMonth() ){
+			return true;
+		}
+		return false;
+	}
+	
 	boolean isSameYear(Date another){
 		if ( this.year == another.getYear() ){
 			return true;
 		}
 		return false;
 	}
+	
+	boolean isSame(Date another){
+		if(this.Date.isSameYear==true || this.Date.isSameMonth==true || this.Date.isSameDay==true){
+			return true;
+		}
+		return false;
+	}
+	
+	
 	
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
